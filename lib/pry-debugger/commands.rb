a55133cb2f13946424649a7df9a153d579832b8c
Jason Wall <javajo@gmail.com>
2014-11-21 12:12:30 -0700

    alias the good stuff

--- lib/pry-debugger/commands.rb
+++ lib/pry-debugger/commands.rb
@@ -243,3 +243,8 @@ module PryDebugger
 end
 
 Pry.commands.import PryDebugger::Commands
+
+Pry.commands.alias_command 'c', 'continue'
+Pry.commands.alias_command 's', 'step'
+Pry.commands.alias_command 'n', 'next'
+Pry.commands.alias_command 'f', 'finish'
